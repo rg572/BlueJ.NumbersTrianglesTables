ae672c5a06ea20b34d49dbbb82b80704e998b0b7
Robert Gordon <robertgordon@zipcoders-MacBook-Pro-4.local>
2019-05-23 13:31:05 -0400

    lab done

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,62 @@
 
-
+import java.lang.StringBuilder;
 
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        StringBuilder sbuild = new StringBuilder();
+        
+        for(int i=0; i<stop;i++){
+            sbuild.append(i);
+        }
+        
+        return sbuild.toString();
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        StringBuilder sbuild = new StringBuilder();
+        
+        for(int i=start;i<stop;i++){
+            sbuild.append(i);
+        }
+        return sbuild.toString();
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder sbuild = new StringBuilder();
+        
+        for(int i=start;i<stop;i+=step){
+            sbuild.append(i);
+        }
+        return sbuild.toString();
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        if(start%2==1){
+            start++;
+        }
+        
+        return getRange(start,stop,2);
+        //return null;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        if(start%2==0){
+            start++;
+        }
+  
+        return getRange(start,stop,2);
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        StringBuilder sbuild = new StringBuilder();
+        
+        for(int i=start;i<=stop;i++){
+            sbuild.append((int)Math.pow(i,exponent));
+        }
+        return sbuild.toString();
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -1,21 +1,32 @@
- 
+import java.lang.StringBuilder;
 
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder sbuild = new StringBuilder();
+        for(int i=0;i<numberOfStars;i++){
+            sbuild.append('*');
+        }
+        return sbuild.toString();
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder sbuild = new StringBuilder();
+        for(int i=1;i<=numberOfRows;i++){
+            sbuild.append(getRow(i));
+            sbuild.append('\n');
+        }
+        return sbuild.toString();
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        //StringBuilder sbuild = new StringBuilder();
+        
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
